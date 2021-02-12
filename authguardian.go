0ce7dab6035b4e5628c68c44d31e251d4bcdf107
whitejokeer <jairosr_1995@hotmail.com>
2021-02-11 21:44:15 -0500

    authentication with Go-Guardian

---
+++ authguardian.go
@@ -0,0 +1,78 @@
+package authguardian
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/shaj13/go-guardian/auth"
+	"github.com/shaj13/go-guardian/auth/strategies/basic"
+	"github.com/shaj13/go-guardian/auth/strategies/bearer"
+	"github.com/shaj13/go-guardian/store"
+)
+
+var authenticator auth.Authenticator
+var cache store.Cache
+
+func setupGoGuardian() {
+	authenticator = auth.New()
+	cache = store.NewFIFO(context.Background(), time.Minute*5)
+
+	basicStrategy := basic.New(validateUser, cache)
+	tokenStrategy := bearer.New(verifyToken, cache)
+
+	authenticator.EnableStrategy(basic.StrategyKey, basicStrategy)
+	authenticator.EnableStrategy(bearer.CachedStrategyKey, tokenStrategy)
+}
+
+func middleware(next http.Handler) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Executing Auth Middleware")
+		user, err := authenticator.Authenticate(r)
+		if err != nil {
+			code := http.StatusUnauthorized
+			http.Error(w, http.StatusText(code), code)
+			return
+		}
+		log.Printf("User %s Authenticated\n", user.UserName())
+		next.ServeHTTP(w, r)
+	})
+}
+
+func createToken(w http.ResponseWriter, r *http.Request) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"iss": "auth-app",
+		"sub": "medium",
+		"aud": "any",
+		"exp": time.Now().Add(time.Minute * 5).Unix(),
+	})
+	jwtToken, _ := token.SignedString([]byte("secret"))
+	w.Write([]byte(jwtToken))
+}
+
+func validateUser(ctx context.Context, r *http.Request, userName, password string) (auth.Info, error) {
+	if userName == "medium" && password == "medium" {
+		return auth.NewDefaultUser("medium", "1", nil, nil), nil
+	}
+	return nil, fmt.Errorf("Invalid credentials")
+}
+
+func verifyToken(ctx context.Context, r *http.Request, tokenString string) (auth.Info, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte("secret"), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		user := auth.NewDefaultUser(claims["medium"].(string), "", nil, nil)
+		return user, nil
+	}
+	return nil, fmt.Errorf("Invaled token")
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/whitejokeer/authguardian
+
+go 1.13
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/shaj13/go-guardian v1.5.11
+)
